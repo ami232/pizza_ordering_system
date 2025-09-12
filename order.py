5739a29bb739afe3cd0ae82f255d95acfac683b8
Alberto Martin <ami232@gmail.com>
2025-09-12 13:54:12 +0200

    Add order.py

---
+++ order.py
@@ -0,0 +1,10 @@
+class Order:
+    def __init__(self, order_id, pizzas):
+        self.order_id = order_id
+        self.pizzas = pizzas
+
+    def __str__(self):
+        order_str = "Order #" + str(self.order_id) + ":\n"
+        for pizza in self.pizzas:
+            order_str += "- " + str(pizza) + "\n"
+        return order_str
