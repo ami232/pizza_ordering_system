324a11f12c8f89addd38ab4c11dbf19cfb768741
Qamar Harith Hussein Aftan <qhussein.ieu2023@student.ie.edu>
2025-09-12 19:10:24 +0200

    add to OrderManager

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from pizza import PizzaFactory
+from order import OrderManager
 
 factory = PizzaFactory()
 
 margherita = factory.create_margherita()
 pepperoni = factory.create_pepperoni()
-veggie = factory.create_custom("Veggie", ["tomato sauce ", "mozzarella", "bell peppers", "olives", "onions"])
+veggie = factory.create_custom(
+    "Veggie",
+    ["tomato sauce", "mozzarella", "bell peppers", "olives", "onions"]
+)
+
+manager = OrderManager()
+order = manager.add_order([margherita, pepperoni, veggie])
 
 print(margherita)
 print(pepperoni)
 print(veggie)
+
+print(f"Created Order #{order.order_id}")
+manager.list_orders()
