0d8f16252dbd15f6d52244605988f9fed68f0c76
Alberto Martin <ami232@gmail.com>
2025-09-12 13:54:31 +0200

    Add OrderManager to order.py

--- order.py
+++ order.py
@@ -8,3 +8,35 @@ class Order:
         for pizza in self.pizzas:
             order_str += "- " + str(pizza) + "\n"
         return order_str
+
+
+class OrderManager:
+    _instance = None
+    orders = []
+    next_order_id: int
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(OrderManager, cls).__new__(cls)
+            cls._instance.orders = []
+            cls._instance.next_order_id = 1
+        return cls._instance
+
+    def add_order(self, pizzas):
+        order_id = self.next_order_id
+        order = Order(order_id, pizzas)
+        self.orders.append(order)
+        self.next_order_id += 1
+        return order
+
+    def list_orders(self):
+        print("Current Orders:")
+        for order in self.orders:
+            print(order)
+        print()
+
+    def prepare_order(self, order_id):
+        pass
+
+    def dispatch_order(self, order_id):
+        pass
